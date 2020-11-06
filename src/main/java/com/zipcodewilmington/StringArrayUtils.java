a87a73c3a05e9224fcc382dffe72d2a63314aa8e
DavidToribio1 <dtmuzik1@gmail.com>
2020-11-06 12:20:45 -0500

    ran test didnt compelete all code

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -9,6 +9,8 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
+
+
         return array[0];
     }
 
@@ -17,6 +19,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
+
         return array[1];
     }
 
@@ -25,7 +28,8 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 1];
     }
 
     /**
@@ -33,8 +37,9 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
+
 
     /**
      * @param array array of String objects
@@ -42,7 +47,16 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+
+        boolean isString = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                isString = true;
+
+            }
+
+        }
+        return isString;
     }
 
     /**
@@ -52,56 +66,71 @@ public class StringArrayUtils {
     public static String[] reverse(String[] array) {
         return null;
     }
+        //String[] reversed = reverse(array);
+        //if (array.equals(reversed)) {
+        // return true;
+        //}
+        // if (array == null || reversed == null) {
+        // return false;
+        // }
+        // Integer n = array.length;
+//        for (Integer i = 0; i < n; i++) {
+        //if (!array[i].equals(reversed[i])) {
+        //    return false;
+        //}
+        // }
+        // return true;
+        // }
 
-    /**
-     * @param array array of String objects
-     * @return true if the order of the array is the same backwards and forwards
-     */ // TODO
-    public static boolean isPalindromic(String[] array) {
-        return false;
+        /**
+         * @param array array of String objects
+         * @return true if the order of the array is the same backwards and forwards
+         */ // TODO
+        public static boolean isPalindromic (String[]array){
+            return false;
+        }
+
+        /**
+         * @param array array of String objects
+         * @return true if each letter in the alphabet has been used in the array
+         */ // TODO
+        public static boolean isPangramic (String[]array){
+            return false;
+        }
+
+        /**
+         * @param array array of String objects
+         * @param value value to check array for
+         * @return number of occurrences the specified `value` has occurred
+         */ // TODO
+        public static int getNumberOfOccurrences (String[]array, String value){
+            return 0;
+        }
+
+        /**
+         * @param array         array of String objects
+         * @param valueToRemove value to remove from array
+         * @return array with identical contents excluding values of `value`
+         */ // TODO
+        public static String[] removeValue (String[]array, String valueToRemove){
+            return null;
+        }
+
+        /**
+         * @param array array of chars
+         * @return array of Strings with consecutive duplicates removes
+         */ // TODO
+        public static String[] removeConsecutiveDuplicates (String[]array){
+            return null;
+        }
+
+        /**
+         * @param array array of chars
+         * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
+         */ // TODO
+        public static String[] packConsecutiveDuplicates (String[]array){
+            return null;
+        }
+
     }
 
-    /**
-     * @param array array of String objects
-     * @return true if each letter in the alphabet has been used in the array
-     */ // TODO
-    public static boolean isPangramic(String[] array) {
-        return false;
-    }
-
-    /**
-     * @param array array of String objects
-     * @param value value to check array for
-     * @return number of occurrences the specified `value` has occurred
-     */ // TODO
-    public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
-    }
-
-    /**
-     * @param array         array of String objects
-     * @param valueToRemove value to remove from array
-     * @return array with identical contents excluding values of `value`
-     */ // TODO
-    public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
-    }
-
-    /**
-     * @param array array of chars
-     * @return array of Strings with consecutive duplicates removes
-     */ // TODO
-    public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
-    }
-
-    /**
-     * @param array array of chars
-     * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
-     */ // TODO
-    public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
-    }
-
-
-}
